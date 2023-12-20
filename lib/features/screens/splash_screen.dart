97e23e0597e6567a1d7e42bca6dd5c824efd2e05
prodigy234 <k.gbenga@gmail.com>
2023-12-20 01:03:04 +0100

    Changes

---
+++ lib/features/screens/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:appclick_fashion_app/authentication/onboarding_screen/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 4), () {
+      Navigator.push(context, MaterialPageRoute(builder: (context) {
+        return const OnboardingScreen();
+      }));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Image.asset(
+          'assets/images/logo.jpg',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
